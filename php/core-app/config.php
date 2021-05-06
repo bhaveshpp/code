a916a6e0f7a74b498dd56f3cf3239e36613b7677
bhaveshpp <bhaveshp.magento@gmail.com>
2021-05-06 09:27:22 +0530

    adding usefull code...

--- php/core-app/config.php
+++ php/core-app/config.php
@@ -8,12 +8,10 @@ define("CONFIG",[
     "SERVER_URL" => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]",
     "SMTP_HOST" => "ssl://smtp.gmail.com",
     "SMTP_PORT" => "465",
-    "SMTP_USER" => "dev11.ts11@gmail.com",
-    "SMTP_PASS" => "tecksky@052820",
-    "SMTP_SENDER" => "Universetech <dev11.ts11@gmail.com>",
-    // "SMTP_SENDER" => "Universetech <info@universe.com>",
-    "SMTP_RECIVER" => "Universetech <dev11.ts11@gmail.com>",
-    // "SMTP_RECIVER" => "universetech <hr.tecksky@gmail.com>",//Default Reciver
+    "SMTP_USER" => "email@gmail.com",
+    "SMTP_PASS" => "secure",
+    "SMTP_SENDER" => "Universetech <info@example.com>",
+    "SMTP_RECIVER" => "Universetech <testing@gmail.com>",
 ]);
 
 function getBaseUrl()
